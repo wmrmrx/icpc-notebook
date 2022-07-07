f2a9b8b1ace02e6a105da1ebb664e37d27a76f33
Nathan Luiz <nathanluiz33@gmail.com>
2022-07-07 14:47:10 -0300

    adicionando template, hash.sh e hashing.cpp

---
+++ extra/template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define IOS ios::sync_with_stdio(0);cin.tie(0)
+
+using ll = long long;
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T){ 
+  cerr << ' ' << H; 
+  dbg_out(T...); 
+}
+#define dbg(...) cerr<<"(" << #__VA_ARGS__<<"):" , dbg_out(__VA_ARGS__) , cerr << endl
+
+void solve(){
+    
+}
+
+signed main(){
+    IOS;
+    solve();
+}
