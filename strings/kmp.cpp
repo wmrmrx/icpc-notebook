7ec043d41e2947493649cf019fc77b60414fe06d
Willian Mori <wnmrmr@gmail.com>
2022-07-28 17:30:03 -0300

    add kmp and z

---
+++ strings/kmp.cpp
@@ -0,0 +1,10 @@
+// b29648
+template <typename T> vector<int> kmp(int sz, const T s[]) {
+	vector<int> pi(sz);
+	for(int i=1;i<sz;i++) {
+		int &j = pi[i];
+		for(j=pi[i-1];j>0 && s[i]!=s[j];j=pi[j-1]);
+		if(s[i] == s[j]) j++;
+	}
+	return pi;
+};
