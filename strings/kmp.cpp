585bb364b12be5018d3e7e88850b25a12787498a
Nathan Luiz <nathanluiz33@gmail.com>
2022-08-06 22:30:08 -0300

    complementando kmp

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -1,3 +1,17 @@
+/* 	
+	KMP
+
+	mathcing(s, t) retorna os indices das ocorrencias
+	de s em t
+	autKMP constroi o automato do KMP
+
+	Complexidades:
+	pi - O(n)
+	match - O(n + m)
+	construir o automato - O(|sigma|*n)
+	n = |padrao| e m = |texto|
+*/
+
 // b29648
 template <typename T> vector<int> kmp(int sz, const T s[]) {
 	vector<int> pi(sz);
@@ -7,4 +21,28 @@ template <typename T> vector<int> kmp(int sz, const T s[]) {
 		if(s[i] == s[j]) j++;
 	}
 	return pi;
+};
+
+// c82524
+template<typename T> vector<int> matching(T& s, T& t) {
+	vector<int> p = pi(s), match;
+	for (int i = 0, j = 0; i < t.size(); i++) {
+		while (j and s[j] != t[i]) j = p[j-1];
+		if (s[j] == t[i]) j++;
+		if (j == s.size()) match.push_back(i-j+1), j = p[j-1];
+	}
+	return match;
+}
+
+// 79bd9e
+struct KMPaut : vector<vector<int>> {
+	KMPaut(){}
+	KMPaut (string& s) : vector<vector<int>>(26, vector<int>(s.size()+1)) {
+		vector<int> p = pi(s);
+		auto& aut = *this;
+		aut[s[0]-'a'][0] = 1;
+		for (char c = 0; c < 26; c++)
+			for (int i = 1; i <= s.size(); i++)
+				aut[c][i] = s[i]-'a' == c ? i+1 : aut[c][p[i-1]];
+	}
 };
