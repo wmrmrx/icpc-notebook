cffd19a3c1e568161ced1d32d9b3addb1c8c2fda
wmrmrx <wmrmrx@gmail.com>
2022-09-04 21:20:20 -0300

    int and double

--- geometry/int.cpp
+++ geometry/int.cpp
@@ -1,33 +1,26 @@
-constexpr double EPS = 1e-10;
-
-bool zero(double x) {
-	return abs(x) <= EPS;
+bool zero(int x) {
+	return x == 0;
 }
 
 // CORNER: point = (0, 0)
 struct point {
-	double x, y;
+	int x, y;
 	
-	point(double x=0, double y=0): x(x), y(y) {}
+	point(int x=0, int y=0): x(x), y(y) {}
 	
 	point operator+(point rhs) { return point(x+rhs.x, y+rhs.y); }
 	point operator-(point rhs) { return point(x-rhs.x, y-rhs.y); }
-	point operator*(double k) { return point(x*k, y*k); }
-	point operator/(double k) { return point(x/k, y/k); }
-	double operator*(point rhs) { return x*rhs.x + y*rhs.y; }
-	double operator^(point rhs) { return x*rhs.y - y*rhs.x; }
+	int operator*(point rhs) { return x*rhs.x + y*rhs.y; }
+	int operator^(point rhs) { return x*rhs.y - y*rhs.x; }
 
-	point rotated(point p, point polar) { return point(*this^polar,*this*polar); }
-	point rotated(point p, double ang) { return rotated(p, point(sin(ang),cos(ang))); }
-	double norm2() { return *this * *this; }
-	double norm() { return sqrt(norm2()); }
+	int norm2() { return *this * *this; }
+
+	using tup = tuple<int, int>;
 
 	bool operator<(const point& rhs) const {
-		return x < rhs.x - EPS || (zero(x-rhs.x) && y < rhs.y - EPS);
+		return tup{x, y} < tup{rhs.x, rhs.y};
 	}
 };
-
-const point ccw90(1, 0), cw90(-1, 0);
 
 // angular comparison in [0, 2pi)
 // smallest is (1, 0)
@@ -37,28 +30,24 @@ bool ang_cmp(point a, point b) {
 		// 0 if ang in [0, pi), 1 if in [pi, 2pi)
 		return p.y < 0 || (p.y == 0 && p.x < 0);
 	};
-	using tup = tuple<bool, double>;
+	using tup = tuple<bool, int>;
 	return tup{quad(a), 0} < tup{quad(b), a^b};
 }
 
-double dist2(point p, point q) { // squared distance
+int dist2(point p, point q) { // squared distance
     return (p - q)*(p - q);
 }
 
-double dist(point p, point q) {
-    return sqrt(dist2(p, q));
-}
-
-double area2(point a, point b, point c) { // two times signed area of triangle abc
+int area2(point a, point b, point c) { // two times signed area of triangle abc
 	return (b - a) ^ (c - a);
 }
 
 bool left(point a, point b, point c) {
-	return area2(a, b, c) > EPS; // counterclockwise
+	return area2(a, b, c) > 0; // counterclockwise
 }
 
 bool right(point a, point b, point c) {
-	return area2(a, b, c) < -EPS; // clockwise
+	return area2(a, b, c) < 0; // clockwise
 }
 
 bool collinear(point a, point b, point c) {
@@ -84,12 +73,12 @@ struct segment {
 	}
 
 	bool contains(point p) {
-		double lx = a.x, ux = b.x, ly = a.y, uy = b.y;
+		int lx = a.x, ux = b.x, ly = a.y, uy = b.y;
 		if(lx > ux) swap(lx, ux); 
 		if(ly > uy) swap(ly, uy);
 		return collinear(a, b, p)
-			&& lx-EPS < p.x && p.x < ux+EPS
-			&& ly-EPS < p.y && p.y < uy+EPS;
+			&& lx <= p.x && p.x <= ux
+			&& ly <= p.y && p.y <= uy;
 	}
 
 	bool intersects(segment rhs) {
@@ -107,18 +96,4 @@ struct segment {
 
 bool parallel(segment r, segment s) {
 	return parallel(r.v(), s.v());
-}
-
-point intersection(segment r, segment s) {
-	if(parallel(r, s)) return point(HUGE_VAL, HUGE_VAL);
-	point vr = r.v(), vs = s.v();
-	double cr = vr ^ r.a, cs = vs ^ s.a;
-	return (vs*cr - vr*cs) / (vr ^ vs);
-}
-
-point proj(segment r, point p) {
-	auto [a, b] = r;
-	p = p - a;
-	point v = b-a;
-	return a + v*((p*v)/(v*v));
 }
