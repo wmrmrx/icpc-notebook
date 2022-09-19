775889c040c5d303ed0c143c2b1a3342a5a53404
wmrmrx <wmrmrx@gmail.com>
2022-09-19 15:15:55 -0300

    scc.cpp

---
+++ code/graphs/scc.cpp
@@ -0,0 +1,46 @@
+// Builds forest of strongly connected components for an UNDIRECTED graph
+// Complexity: O(N+M)
+// e15c9a
+struct SCC {
+	vector<int> comp; // comp[v]: component of vertex v
+
+	int ncomp; // number of components
+	vector<int> sz; // sz[i]: size of component i
+	vector<vector<int>> gc; // gc[i]: list of adjacent componentes
+				
+	SCC(int n, vector<int> g[]): comp(n, -1), ncomp(0) {
+		vector<bool> vis(n, false);
+		vector<int> low(n), prof(n);
+
+		auto dfs = [&](auto& self, int v, int dad = -1) -> void {
+			vis[v] = 1;
+			for(int p: g[v]) if(p != dad) {
+				if(!vis[p]) {
+					low[p] = prof[p] = prof[v] + 1;
+					self(self, p, v);
+					low[v] = min(low[v], low[p]);
+				} else low[v] = min(low[v], prof[p]);
+			}
+			if(low[v] == prof[v]) ncomp++;
+		};
+		for(int i=0;i<n;i++) if(!vis[i]) dfs(dfs, i);
+
+		sz.resize(ncomp); gc.resize(ncomp);
+
+		int cnt = 0;
+		auto build = [&](auto& self, int v, int c = -1) -> void {
+			if(low[v] == prof[v]) c = cnt++;
+			comp[v] = c;
+			sz[c]++;
+			for(int p: g[v]) if(comp[p] == -1) {
+				self(self, p, c);
+				int pc = comp[p];
+				if(c != pc) {
+					gc[c].push_back(pc);
+					gc[pc].push_back(c);
+				}
+			}
+		};
+		for(int i=0;i<n;i++) if(comp[i] == -1) build(build, i);
+	}
+};
