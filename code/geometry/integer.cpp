d1b2222eebad6b282cf69b92f1808dd1dac2f6e2
wmrmrx <wmrmrx@gmail.com>
2022-09-21 19:20:08 -0300

    fix

--- code/geometry/integer.cpp
+++ code/geometry/integer.cpp
@@ -62,7 +62,7 @@ bool collinear(point a, point b, point c) {
 
 // CORNER: a || b == (0, 0)
 int parallel(point a, point b) {
-	if((a ^ b) != 0) return 0;
+	if(!zero(a ^ b)) return 0;
 	return (a.x>0) == (b.x>0) && (a.y > 0) == (b.y > 0) ? 1 : -1;
 }
 
