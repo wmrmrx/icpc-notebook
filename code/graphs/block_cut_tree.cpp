5fab9b7fc43a9f7c58ead375be3c5584b73ae3bc
wmrmrx <wmrmrx@gmail.com>
2022-09-25 16:02:10 -0300

    remove unneeded stuff

--- code/graphs/block_cut_tree.cpp
+++ code/graphs/block_cut_tree.cpp
@@ -3,22 +3,18 @@
 // Builds forest of block cut trees for an UNDIRECTED graph
 // Constructor: SCC(|V|, |E|, [[v, e]; |V|])
 // Complexity: O(N+M)
-// ea9e08
+// f8962a
 struct BlockCutTree {
-	vector<bool> art; // art[v]: true if v is an articulation point
-	vector<int> comp; // comp[e]: component of edge e
-
 	int ncomp; // number of components
-	vector<int> sz; // sz[c]: size of component c (number of edges)
-	vector<vector<int>> gc; // gc[c]: list of articulation points a component c is adjacent to
+	vector<int> comp; // comp[e]: component of edge e
 	vector<vector<int>> gart; // gart[v]: list of components an articulation point v is adjacent to
-				  // if v is NOT an articulation point, then gart[v] is empty
+			          // if v is NOT an articulation point, then gart[v] is empty
 				
 	template <typename E>
-	BlockCutTree(int n, int m, vector<E> g[]): art(n), comp(m, -1), ncomp(0), gart(n) {
+	BlockCutTree(int n, int m, vector<E> g[]): ncomp(0), comp(m), gart(n) {
 		vector<bool> vis(n), vise(m);
 		vector<int> low(n), prof(n);
-		vector<pair<int,int>> st;
+		stack<pair<int,int>> st;
 
 		auto dfs = [&](auto& self, int v, bool root = 0) -> void {
 			vis[v] = 1;
@@ -26,7 +22,7 @@ struct BlockCutTree {
 			for(auto [p, e]: g[v]) if(!vise[e]) {
 				vise[e] = 1;
 				int in = st.size();
-				st.emplace_back(e, vis[p] ? -1 : p);
+				st.emplace(e, vis[p] ? -1 : p);
 				if(!vis[p]) {
 					arb++;
 					low[p] = prof[p] = prof[v] + 1;
@@ -34,28 +30,21 @@ struct BlockCutTree {
 					low[v] = min(low[v], low[p]);
 				} else low[v] = min(low[v], prof[p]);
 				if(low[p] >= prof[v]) {
-					art[v] = 1;
 					gart[v].push_back(ncomp);
-					sz.push_back(0);
 					while(st.size() > in) {
-						auto [es, ps] = st.back();
-						sz[ncomp]++;
+						auto [es, ps] = st.top();
 						comp[es] = ncomp;
-						if(ps != -1 && art[ps])
+						if(ps != -1 && !gart[ps].empty())
 							gart[ps].push_back(ncomp);
-						st.pop_back();
+						st.pop();
 					}
 					ncomp++;
 				}
 			}
 			if(root && arb <= 1) {
-				art[v] = false;
 				gart[v].clear();
 			}
 		};
 		for(int v=0;v<n;v++) if(!vis[v]) dfs(dfs, v, 1);
-
-		gc.resize(ncomp);
-		for(int v=0;v<n;v++) for(int c: gart[v]) gc[c].push_back(v);
 	}
 };
