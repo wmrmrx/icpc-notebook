c0076ad20eae169efaf41dc97767b5eb3c3e2dc5
wmrmrx <wmrmrx@gmail.com>
2022-10-01 16:33:38 -0300

    fix convex hull

--- code/geometry/convex_hull.cpp
+++ code/geometry/convex_hull.cpp
@@ -1,23 +1,24 @@
 // Works with double and integer
+// Returns in CCW order (reversed in x in UPPER)
 // Complexity: O(NlogN)
-// c3b176
+// 8909b5
 template <bool UPPER>
 vector<point> hull(vector<point> v) {
 	vector<point> res;
-	if(UPPER) for(auto& p: v) p.y = -p.y;
+	if(UPPER) for(auto& p: v) p.x = -p.x, p.y = -p.y;
 	sort(all(v));
 	for(auto& p: v) {
 		if(res.empty()) { res.push_back(p); continue; }
 		if(res.back().x == p.x) continue;
 		while(res.size() >= 2) {
 			point a = res[res.size()-2], b = res.back();
-			if(!right(a, b, p)) res.pop_back();
+			if(!left(a, b, p)) res.pop_back();
 			//to include collinear points
 			//if(right(a, b, p)) res.pop_back();
 			else break;
 		}
 		res.push_back(p);
 	}
-	if(UPPER) for(auto &p: res) p.y = -p.y;
+	if(UPPER) for(auto& p: res) p.x = -p.x, p.y = -p.y;
 	return res;
 }
