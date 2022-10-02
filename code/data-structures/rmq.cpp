bdfdccd672043da21ff212a44f284e55299eb341
wmrmrx <wmrmrx@gmail.com>
2022-10-02 16:48:23 -0300

    squash

--- code/data-structures/rmq.cpp
+++ code/data-structures/rmq.cpp
@@ -1,18 +1,22 @@
-// c2e4d3
+// 38673e
 template <typename T> struct RMQ {
 	vector<vector<T>> dp;
 	T ops(T a, T b) { return min(a,b); }
 	RMQ() {}
-	RMQ(int sz, T v[]) {
-		int log = 64-__builtin_clzll(sz);
-		dp.assign(log, vector<T>(sz));
-		for(int i=0;i<sz;i++) dp[0][i] = v[i];
-		for(int l=1;l<log;l++) for(int i=0;i<sz;i++) 
-			dp[l][i] = ops(dp[l-1][i],dp[l-1][min(i+(1<<(l-1)), sz-1)]);
+	RMQ(vector<T> v) {
+		int n = v.size();
+		int log = 32-__builtin_clz(n);
+		dp.assign(log, vector<T>(n));
+		copy(all(v), dp[0].begin());
+		for(int l=1;l<log;l++) for(int i=0;i<n;i++) {
+			auto &cur = dp[l], &ant = dp[l-1];
+			cur[i] = ops(ant[i], ant[min(i+(1<<(l-1)), n-1)]);
+		}
 	}
 	T query(int a, int b)  {
 		if(a == b) return dp[0][a];
-		int pot = 63-__builtin_clzll(b-a);
-		return ops(dp[pot][a], dp[pot][b-(1<<pot)+1]);
+		int p = 31-__builtin_clz(b-a);
+		auto &cur = dp[p];
+		return ops(cur[a], cur[b-(1<<p)+1]);
 	}
 };
