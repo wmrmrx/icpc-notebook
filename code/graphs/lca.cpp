d9e379c31132e2acf27c54c1e4d07323b3f7b83f
wmrmrx <wmrmrx@gmail.com>
2022-10-10 11:26:37 -0300

    virtual_tree

--- code/graphs/lca.cpp
+++ code/graphs/lca.cpp
@@ -1,19 +1,18 @@
-// 4d58fb
+// 7885f2
 struct LCA {
-	vector<int> pre; // preorder traversal
+	vector<int> pre, dep; // preorder traversal and depth
 	RMQ<pair<int,int>> rmq;
 
 	LCA() {}
-	LCA(int sz, vector<int> g[], int root): t(sz) {
+	LCA(int sz, vector<int> g[], int root): pre(sz), dep(sz) {
 		vector<pair<int,int>> tour; tour.reserve(2*sz-1);
-		vector<int> prof(sz); 
 		auto dfs = [&](int v, int dad, auto& self) -> void {
 			pre[v] = tour.size();
-			tour.push_back({prof[v],v});
+			tour.push_back({dep[v],v});
 			for(int p: g[v]) if(p != dad) {
-				prof[p] = prof[v]+1;
+				dep[p] = dep[v]+1;
 				self(p,v,self);
-				tour.push_back({prof[v],v});
+				tour.push_back({dep[v],v});
 			}
 		};
 		dfs(root, root, dfs);
@@ -23,5 +22,10 @@ struct LCA {
 	int query(int a, int b) {
 		if(pre[a] > pre[b]) swap(a,b);
 		return rmq.query(pre[a],pre[b]).second;
+	}
+
+	int dist(int a, int b) {
+		int c = query(a,b);
+		return dep[a] + dep[b] - 2*dep[c];
 	}
 };
---
+++ code/graphs/virtual_tree.cpp
@@ -0,0 +1,23 @@
+// 8467af
+namespace vtree {
+	vector<int> vg[MAX];
+
+	// receives list of vertexes and returns root of virtual tree
+	// v must NOT be empty
+	int build(vector<int> v, LCA& lca) {
+		auto cmp = [&](int i, int j) {
+			return lca.pre[i] < lca.pre[j];
+		};
+		sort(all(v), cmp);
+		for(int i=0;i<v.size()-1;i++) v.push_back(lca.query(v[i], v[i+1]));
+		sort(all(v), cmp);
+		v.resize(unique(all(v))-v.begin());
+		for(auto x: v) vg[x].clear();
+		for(int i=1;i<v.size();i++) {
+			int dad = lca.query(v[i-1], v[i]);
+			vg[dad].push_back(v[i]);
+			vg[v[i]].push_back(dad);
+		}
+		return v[0];
+	}
+}
