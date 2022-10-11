cfc9cba31ec78dee1010397f694795409a78ff5c
wmrmrx <wmrmrx@gmail.com>
2022-10-10 21:07:23 -0300

    bugfixes

--- code/graphs/virtual_tree.cpp
+++ code/graphs/virtual_tree.cpp
@@ -1,23 +1,24 @@
-// 5badc5
+// ea97fc
 namespace vtree {
 	vector<int> vg[MAX];
 
 	// receives list of vertexes and returns root of virtual tree
 	// v must NOT be empty
-	int build(vector<int> v, LCA& lca) {
+	int build(vector<int> vs, LCA& lca) {
 		auto cmp = [&](int i, int j) {
 			return lca.pre[i] < lca.pre[j];
 		};
-		sort(all(v), cmp);
-		for(int i=0;i+1<v.size();i++) v.push_back(lca.query(v[i], v[i+1]));
-		sort(all(v), cmp);
-		v.resize(unique(all(v))-v.begin());
-		for(auto x: v) vg[x].clear();
-		for(int i=1;i<v.size();i++) {
-			int dad = lca.query(v[i-1], v[i]);
-			vg[dad].push_back(v[i]);
-			vg[v[i]].push_back(dad);
+		sort(all(vs), cmp);
+		for(int i=vs.size()-1; i>0; i--) vs.push_back(lca.query(vs[i-1], vs[i]));
+		sort(all(vs));
+		vs.resize(unique(all(vs))-vs.begin());
+		sort(all(vs), cmp);
+		for(auto v: vs) vg[v].clear();
+		for(int i=1;i<vs.size();i++) {
+			int dad = lca.query(vs[i-1], vs[i]);
+			vg[dad].push_back(vs[i]);
+			vg[vs[i]].push_back(dad);
 		}
-		return v[0];
+		return vs[0];
 	}
 }
