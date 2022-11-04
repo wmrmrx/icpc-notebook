1ebee94a5209fe968df716009df22dacf1c10c52
wmrmrx <wmrmrx@gmail.com>
2022-11-03 22:22:39 -0300

    fix ordered_set

--- code/extra/ordered_set.cpp
+++ code/extra/ordered_set.cpp
@@ -1,5 +1,6 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 // iterator find_by_order(size_t index), size_t order_of_key(T key)
 template <typename T> 
-using ordered_set=__gnu_pbds::tree<T, __gnu_pbds::null_type,std::less<T>, __gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>;
+using ordered_set=tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
