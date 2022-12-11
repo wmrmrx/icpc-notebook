40af60f1e394fbddc8e8ba098bb36c0eb632046b
Nathan Luiz <nathanluiz33@gmail.com>
2022-12-11 15:43:21 -0300

    atualizando template de segpersistente

--- code/data-structures/segtreePersistente.cpp
+++ code/data-structures/segtreePersistente.cpp
@@ -1,61 +1,70 @@
-/* Persistent segment tree. This example is for queries of sum in range
- * and updates of sum in position, but any query or update can be achieved
- * changing the NEUT value, and functions updNode and merge.
- * The version 0 of the persistent segTree has implicitly an array of
- * length n full of NEUT values.
- *
- * It's recommend to set n as the actual length of the array.
- * ```
- * int n; cin >> n;
- * segTree::n = n;
- * ```
- *
+/* Persistent segment tree.
+ * 
  * Complexity: O(logn) memory and time per query/update
- * 50e1d1
+ * fc63b9
  */
 
-const int NEUT = 0;
-
-struct segTree {
-	vector<int> t = vector<int>(1, NEUT);
-	vector<int> left = vector<int>(1, 0), right = vector<int>(1, 0);
-	static int n;
-	int newNode(int v, int l=0, int r=0) {
-		t.pb(v), left.pb(l), right.pb(r);
-		return sz(t) - 1;
-	}
-	int merge(int a, int b) {
-		return a + b;
-	}
-	// Initializes a segTree with the values of the array A of length n
-	int init(int* A, int L=0, int R=n) {
-		if(L + 1 == R) return newNode(A[L]);
-		int M = (L + R) >> 1;
-		int l = init(A, L, M), r = init(A, M, R);
-		return newNode(merge(t[l], t[r]), l , r);
-	}
-	int updNode(int cur_value, int upd_value) {
-		return cur_value + upd_value;
-	}
-	// updates the position pos of version k with the value v
-	int upd(int k, int pos, int v, int L=0, int R=n) {
-		int nxt = newNode(t[k], left[k], right[k]);
-		if(L + 1 == R) t[nxt] = updNode(t[nxt], v);
-		else {
-			int M = (L + R) >> 1;
-			int temp;
-			if(pos < M) temp = upd(left[nxt], pos, v, L, M), left[nxt] = temp;
-			else temp = upd(right[nxt], pos, v, M, R), right[nxt] = temp;
-			t[nxt] = merge(t[left[nxt]], t[right[nxt]]);
-		}
-		return nxt;
-	}
-	// query in the range [l, r) of version k
-	int que(int k, int l, int r, int L=0, int R=n){
-		if(r <= L || R <= l) return NEUT;
-		if(l <= L && R <= r) return t[k];
-		int M = (L + R) >> 1;
-		return merge(que(left[k], l, r, L, M), que(right[k], l, r, M, R));
-	}
+template<class T, int SZ> struct pseg {
+    static const int LIMIT = 1e7; // adjust
+    int l[LIMIT], r[LIMIT], nex = 0;
+    T val[LIMIT];
+    
+    //// HELPER
+    int copy(int cur) {
+        int x = nex++;
+        val[x] = val[cur], l[x] = l[cur], r[x] = r[cur]; // lazy[x] = lazy[cur];
+        return x;
+    }
+    T comb(T a, T b) { return a+b; }
+    void pull(int x) { val[x] = comb(val[l[x]],val[r[x]]); } 
+    /*void push(int cur, int L, int R) { 
+        if (!lazy[cur]) return;
+        if (L != R) {
+            l[cur] = copy(l[cur]);
+            val[l[cur]] += lazy[cur];
+            lazy[l[cur]] += lazy[cur];
+            
+            r[cur] = copy(r[cur]);
+            val[r[cur]] += lazy[cur];
+            lazy[r[cur]] += lazy[cur];
+        }
+        lazy[cur] = 0;
+    }*/
+ 
+    //// MAIN FUNCTIONS
+    T query(int cur, int lo, int hi, int L, int R) {  
+        if (lo <= L && R <= hi) return val[cur];
+        if (R < lo || hi < L) return 0;
+        int M = (L+R)/2;
+        return comb(query(l[cur],lo,hi,L,M), query(r[cur],lo,hi,M+1,R));
+    }
+    int upd(int cur, int pos, T v, int L, int R) {
+        if (R < pos || pos < L) return cur;
+        
+        int x = copy(cur);
+        if (pos <= L && R <= pos) { val[x] = v; return x; }
+        
+        int M = (L+R)/2;
+        l[x] = upd(l[x],pos,v,L,M), r[x] = upd(r[x],pos,v,M+1,R);
+        pull(x); return x;
+    }
+    int build(vector<T>& arr, int L, int R) {
+        int cur = nex++;
+        if (L == R) {
+            if (L < (int) arr.size ()) val[cur] = arr[L];
+            else val[cur] = 0; // array pode ficar com lixo!!!
+            return cur;
+        }
+        
+        int M = (L+R)/2;
+        l[cur] = build(arr,L,M), r[cur] = build(arr,M+1,R);
+        pull(cur); 
+        return cur;
+    }
+    
+    //// PUBLIC
+    vector<int> loc;
+    // void upd(int lo, T v) { loc.pb(upd(loc.back(),lo,v,0,SZ-1)); }
+    // T query(int ti, int lo, int hi) { return query(loc[ti],lo,hi,0,SZ-1); }
+    void build(vector<T>& arr) { loc.pb(build(arr,0,SZ-1)); }
 };
-int segTree::n = N;
