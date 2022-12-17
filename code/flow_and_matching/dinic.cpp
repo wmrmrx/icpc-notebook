75cf40a54f8ce753fb2ea6d7b323d179a3742141
wmrmrx <wmrmrx@gmail.com>
2022-12-17 10:27:46 -0300

    small changes

--- code/flow_and_matching/dinic.cpp
+++ code/flow_and_matching/dinic.cpp
@@ -1,11 +1,13 @@
 // add_edge(s, t, cap): Adds a directed edge from s to t with capacity cap
-// max_flow(s, t): Returns max flow with source s and sink t
+// get_flow(s, t): Returns max flow with source s and sink t
 //
 // Complexity: O(E*V^2). If unit edges only: O(E*sqrt(V))
-// f5f7ec
+// 360dcf
 struct Dinic {
+	const int INF = 1e18;
 	struct edge {
 		int to, cap, flow;
+		edge(int _to, int _cap, int _flow): to(_to), cap(_cap), flow(_flow) {}
 	};
 
 	vector<vector<int>> g;
@@ -17,23 +19,24 @@ struct Dinic {
 	void add_edge(int s, int t, int cap) {
 		int id = e.size();
 		g[s].push_back(id);
-		e.push_back({t, cap, 0});
+		e.emplace_back(t, cap, 0);
 		g[t].push_back(++id);
-		e.push_back({s, cap, cap});
+		e.emplace_back(s, cap, cap);
 	}
 
 	bool bfs(int s, int t) {
 		fill(all(lvl), INF);
 		lvl[s] = 0;
-		queue<int> q{{s}};
+		queue<int> q;
+		q.push(s);
 		while(!q.empty() && lvl[t] == INF) {
-			int cur = q.front();
+			int v = q.front();
 			q.pop();
-			for(int id: g[cur]) {
-				auto &[p, cap, flow] = e[id];
+			for(int id: g[v]) {
+				auto [p, cap, flow] = e[id];
 				if(lvl[p] != INF || cap == flow)
 					continue;
-				lvl[p] = lvl[cur] + 1;
+				lvl[p] = lvl[v] + 1;
 				q.push(p);
 			}
 		}
@@ -47,26 +50,26 @@ struct Dinic {
 			int id = g[v][st[v]];
 			auto &[p, cap, flow] = e[id];
 			if(lvl[v]+1 != lvl[p] || cap == flow) continue;
-			int pushed = dfs(p, min(cap-flow, pool) , t, st);
-			if(pushed) {
-				flow += pushed;
-				e[id^1].flow -= pushed;
-				return pushed;
+			int f = dfs(p, min(cap-flow, pool) , t, st);
+			if(f) {
+				flow += f;
+				e[id^1].flow -= f;
+				return f;
 			}
 		}
 		return 0;
 	}
 
-	int max_flow(int s, int t) {
-		int total_flow = 0;
+	int get_flow(int s, int t) {
+		//reset to initial state
+		//for(int i=0;i<e.size();i++) e[i].flow = (i&1) ? e[i].cap : 0;
+		int res = 0;
 		vector<int> start(g.size());
 		while(bfs(s,t)) {
 			fill(all(start), 0);
-			while(int pushed = dfs(s,INF,t,start)) 
-				total_flow += pushed;
+			while(int f = dfs(s,INF,t,start)) 
+				res += f;
 		}
-		//reset to initial state
-		//for(int i=0;i<e.size();i++) e[i].flow = (i&1) ? e[i].cap : 0;
-		return total_flow;
+		return res;
 	}
 };
