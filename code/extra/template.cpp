8ccd0d59334ffeb8d160c96051541be4bd2a003f
wmrmrx <wmrmrx@gmail.com>
2022-12-18 11:00:18 -0300

    fix typo

--- code/extra/template.cpp
+++ code/extra/template.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void dbg_out() { cerr << endl; }
 template <typename H, typename... T>
 void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
-#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg(__VA_ARGS__); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
 
 void solve() {
 }
