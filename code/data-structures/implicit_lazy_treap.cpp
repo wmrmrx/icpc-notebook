a88f72a6d75df2b7d1bf109d173ef362303dca0e
wmrmrx <wmrmrx@gmail.com>
2023-01-11 23:44:27 -0300

    rewrite implicit_lazy_treap.cpp

--- code/data-structures/implicit_lazy_treap.cpp
+++ code/data-structures/implicit_lazy_treap.cpp
@@ -1,146 +1,136 @@
 // All operations are O(log N)
 // If changes need to be made in lazy propagation,
-// see Treap::reverse() and change Treap::no::prop()
+// see Treap::push() and Treap::pull()
 //
 // Important functions:
-// Treap::insert(T val, int idx)
-// Treap::erase(int idx)
+// Treap::insert(int ind, T info)
+// Treap::erase(int ind)
 // Treap::reverse(int l, int r)
-// Treap::operator[](int idx)
+// Treap::operator[](int ind)
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-// HASH FROM HERE:
-// c018fe
+// HASH FROM HERE
+// cc3c5c
 template <typename T>
 struct Treap {
-	struct no {
-		array<no*, 2> c;
-		T dat;
-		int cnt, h;
-
-		// Example: reverse interval
+	struct node {
+		T info;
+		int l, r, sz;
+		uint64_t h;
 		bool rev;
-
-		no(T dat=T()): c({0, 0}), dat(dat), cnt(1), h(rng()), rev(0) {}
-
-		// propagate
-		void prop() {
-			if(rev) {
-				swap(c[0], c[1]);
-				for(no* x: c) if(x) x->rev = !x->rev;
-				rev = 0;
-			}
-		}
-
-		// refresh
-		no* ref() {
-			cnt = 1;
-			for(no* x: c) if(x) {
-				x->prop();
-				cnt += x->cnt;
-			}
-			return this;
-		}
-
-		// left child size
-		int l() {
-			return c[0] ? c[0]->cnt : 0;
-		}
+		node() {}
+		node(T _info): info(_info), l(0), r(0), sz(1), h(rng()), rev(0) {}
 	};
 
-	int sz;
-	no *root;
-	unique_ptr<no[]> arena;
-
-	// prealloc: number of new_no() calls that will be made in total
-	Treap(int prealloc): sz(0), root(0), arena(new no[prealloc]) {}
-
-	no* new_no(T dat) {
-		arena[sz] = no(dat);
-		return &arena[sz++];
+	int root, ptr;
+	unique_ptr<node[]> v;
+	// max: maximum number of insertions
+	Treap(int max): root(0), ptr(0), v(new node[max+1]) {
+		// v[0] is a placeholder node such that v[0].sz = 0
+		v[0].sz = 0;
 	}
 
-	int cnt(no* x) { return x ? x->cnt : 0; }
+	void push(int nd) {
+		node &x = v[nd];
+		if(x.rev) {
+			swap(x.l, x.r);
+			v[x.l].rev ^= 1;
+			v[x.r].rev ^= 1;
+			x.rev = 0;
+		}
+	}
+	void pull(int nd) {
+		node& x = v[nd];
+		x.sz = v[x.l].sz + v[x.r].sz + 1;
+	}
+	int new_node(T info) {
+		v[++ptr] = node(info);
+		return ptr;
+	}
+	int getl(int nd) {
+		return v[v[nd].l].sz;
+	}
 
-	void merge(array<no*, 2> c, no*& res) {
-		if(!c[0] || !c[1]) {
-			res = c[0] ? c[0] : c[1];
+	void merge(int l, int r, int& res) {
+		if(!l || !r) {
+			res = l + r;
 			return;
 		}
-		for(no* x: c) x->prop();
-		int i = c[0]->h < c[1]->h;
-		no *l = c[i]->c[!i], *r = c[!i];
-		if(i) swap(l, r);
-		merge({l, r}, c[i]->c[!i]);
-		res = c[i]->ref();
+		push(l); push(r);
+		if(v[l].h > v[r].h) {
+			res = l;
+			merge(v[l].r, r, v[l].r);
+		} else {
+			res = r;
+			merge(l, v[r].l, v[r].l);
+		}
+		pull(res);
 	}
-
 	// left treap has size pos
-	void split(no* x, int pos, array<no*, 2>& res, int ra = 0) {
-		if(!x) {
-			res.fill(0);
+	void split(int nd, int &l, int &r, int pos, int ra = 0) {
+		if(!nd) {
+			l = r = 0;
 			return;
 		}
-		x->prop();
-		ra += x->l();
-		int i = pos > ra;
-		split(x->c[i], pos, res, ra+(i?1:-x->l()));
-		x->c[i] = res[!i];
-		res[!i] = x->ref();
+		push(nd);
+		if(pos <= ra + getl(nd)) {
+			split(v[nd].l, l, r, pos, ra);
+			v[nd].l = r;
+			r = nd;
+		} else {
+			split(v[nd].r, l, r, pos, ra + getl(nd) + 1);
+			v[nd].r = l;
+			l = nd;
+		}
+		pull(nd);
 	}
 
 	// Merges all s and makes them root
 	template <int SZ>
-	void merge(array<no*, SZ> s) {
+	void merge(array<int, SZ> s) {
 		root = s[0];
 		for(int i=1;i<SZ;i++)
-			merge({root, s[i]}, root);
+			merge(root, s[i], root);
 	}
 
 	// Splits root into SZ EXCLUSIVE intervals
 	// [0..s[0]), [s[0]..s[1]), [s[1]..s[2])... [s[SZ-1]..end)
 	// Example: split<2>({l, r}) gets the exclusive interval [l, r)
 	template <int SZ>
-	array<no*, SZ> split(array<int, SZ-1> s) {
-		array<no*, SZ> res;
-		array<no*, 2> aux;
-		split(root, s[0], aux);
-		res[0] = aux[0]; res[1] = aux[1];
+	array<int, SZ> split(array<int, SZ-1> s) {
+		array<int, SZ> res;
+		split(root, res[0], res[1], s[0]);
 		for(int i=1;i<SZ-1;i++) {
-			split(res[i], s[i]-s[i-1], aux);
-			res[i] = aux[0]; res[i+1] = aux[1];
+			split(res[i], res[i], res[i+1], s[i]-s[i-1]);
 		}
-		root = nullptr;
+		root = 0;
 		return res;
 	}
 
-	void insert(T val, int idx) {
-		auto s = split<2>({idx});
-		merge<3>({s[0], new_no(val), s[1]});
+	void insert(int ind, T info) {
+		auto s = split<2>({ind});
+		merge<3>({s[0], new_node(info), s[1]});
 	}
-	
-	void erase(int idx) {
-		auto s = split<3>({idx, idx+1});
+	void erase(int ind) {
+		auto s = split<3>({ind, ind+1});
 		merge<2>({s[0], s[2]});
 	}
-
 	// Inclusive
 	void reverse(int l, int r) {
 		auto s = split<3>({l, r+1});
-		s[1]->rev = !s[1]->rev;
+		v[s[1]].rev ^= 1;
 		merge<3>(s);
 	}
-
-	T operator[](int idx) {
-		no* x = root;
-		//assert(0 <= idx && idx < x->cnt);
-		x->prop();
-		for(int ra = x->l(); ra != idx; ra += x->l()) {
-			if(ra < idx) ra++, x = x->c[1];
-			else ra -= x->l(), x = x->c[0];
-			x->prop();
+	T operator[](int ind) {
+		int nd = root;
+		//assert(0 <= ind && ind < x->sz);
+		push(nd);
+		for(int ra=0, nra=getl(nd); nra != ind; nra = ra + getl(nd)) {
+			if(nra < ind) ra = nra + 1, nd = v[nd].r;
+			else nd = v[nd].l;
+			push(nd);
 		}
-		return x->dat;
+		return v[nd].info;
 	}
 };
