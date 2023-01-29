98c90ce02d369aae64f3219574041b0de8386484
Nathan Luiz <nathanluiz33@gmail.com>
2023-01-28 21:10:05 -0300

    adding stable marriage and updating hash.sh

--- code/extra/hash.sh
+++ code/extra/hash.sh
@@ -1,3 +1,4 @@
 # Para usar (hash das linhas [l1, l2]):
 # ./hash.sh arquivo.cpp l1 l2
+# hashing arquivo empty: d41d8c
 sed -n $2','$3' p' $1 | sed '/^#w/d' | cpp -dD -P -fpreprocessed  | tr -d '[:space:]' | md5sum | cut -c-6
