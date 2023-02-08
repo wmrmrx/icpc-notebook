95e98be63fed2a128ddc248c9670926479a77443
Willian Wang <git@willian.wang>
2023-02-08 14:51:55 -0300

    Adiciona hash cumulativo

--- code/extra/hash.sh
+++ code/extra/hash.sh
@@ -1,4 +1,10 @@
 # Para usar (hash das linhas [l1, l2]):
 # ./hash.sh arquivo.cpp l1 l2
 # md5sum do hash.sh: 9cd1295ed4344001c20548b1d6eb55b2
+#
+# Hash acumulativo, linha por linha:
+# for i in $(seq $2 $3); do
+#   echo -n "$i "
+#   sed -n $2','$i' p' $1 | cpp -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6
+# done
 sed -n $2','$3' p' $1 | cpp -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6
