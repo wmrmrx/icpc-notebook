c9b847183de3ecafb64adffa479ce44436611302
wmrmrx <wmrmrx@gmail.com>
2023-02-08 14:29:34 -0300

    hash.sh md5sum

--- code/extra/hash.sh
+++ code/extra/hash.sh
@@ -1,4 +1,4 @@
 # Para usar (hash das linhas [l1, l2]):
 # ./hash.sh arquivo.cpp l1 l2
-# hashing arquivo empty: d41d8c
-sed -n $2','$3' p' $1 | cpp -dD -P -fpreprocessed  | tr -d '[:space:]' | md5sum | cut -c-6
+# md5sum do hash.sh: 9cd1295ed4344001c20548b1d6eb55b2
+sed -n $2','$3' p' $1 | cpp -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6
