69e08391904a102729f524eb39ef11be7b5e53d3
wmrmrx <wmrmrx@gmail.com>
2024-02-15 20:14:29 -0300

    Change Dinic

--- code/flow-and-matching/dinic.cpp
+++ code/flow-and-matching/dinic.cpp
@@ -4,6 +4,12 @@
 //
 // Complexity: O(E*V^2). If unit edges only: O(E*sqrt(V))
 
+// Dinitz
+// add_edge(s, t, cap): Adds a directed edge from s to t with capacity cap
+// get_flow(s, t): Returns max flow with source s and sink t
+//
+// Complexity: O(E*V^2). If unit edges only: O(E*sqrt(V))
+
 bool zero(int x) {
 	return x == 0;
 }
@@ -15,54 +21,33 @@ template<typename F>
 struct Dinic {
 	static constexpr F INF = numeric_limits<F>::max();
 	struct edge {
-		int to;
+		int to, nxt;
 		F cap, flow;
+		F f() { return cap - flow; }
 	};
 
 	int n, s, t;
-	vector<vector<int>> adj;
-	vector<int> lvl;
+	vector<int> beg, lvl;
 	vector<edge> g;
 
-	Dinic(int sz): n(sz), adj(sz), lvl(sz) {}
+	Dinic(int sz): n(sz), beg(sz, -1), lvl(sz) {}
 
 	void add_edge(int u, int v, F cap) {
 		int id = g.size();
-		adj[u].pb(id);
-		adj[v].pb(++id);
-		g.pb({v, cap, 0});
-		g.pb({u, cap, cap});
+		g.pb({v, beg[u], cap, 0});
+		g.pb({u, beg[v], cap, cap});
+		beg[u] = id; 
+		beg[v] = id + 1;
 	}
 
-	bool bfs() {
-		fill(all(lvl), n);
-		lvl[s] = 0;
-		queue<int> q;
-		q.push(s);
-		while(!q.empty() && lvl[t] == n) {
-			int u = q.front();
-			q.pop();
-			for(int id: adj[u]) {
-				auto [v, cap, flow] = g[id];
-				if( lvl[v] != n || zero(cap - flow) )
-					continue;
-				lvl[v] = lvl[u] + 1;
-				q.push(v);
-			}
-		}
-		return lvl[t] != n;
-	}
-
-	F dfs(int u, F pool, vector<size_t>& st) {
-		if(zero(pool)) return 0;
-		if(u == t) return pool;
-		for( ; st[u]<adj[u].size() ; st[u]++) {
-			int id = adj[u][st[u]];
-			auto &[v, cap, flow] = g[id];
-			if( lvl[u] + 1 != lvl[v] || zero(cap - flow) ) continue;
-			int f = dfs(v, min(cap-flow, pool) , st);
-			if(f) {
-				flow += f;
+	F dfs(int u, F pool, vector<int>& ptr) {
+		if(zero(pool) || u == t) return pool;
+		for(int &id = ptr[u]; ~id; id = g[id].nxt) {
+			auto &e = g[id];
+			if( lvl[u] + 1 != lvl[e.to] || zero(e.f()) ) continue;
+			F f;
+			if(not zero( f = dfs(e.to, min(e.f(), pool), ptr) )) {
+				g[id].flow += f;
 				g[id^1].flow -= f;
 				return f;
 			}
@@ -74,14 +59,26 @@ struct Dinic {
 		//reset to initial state
 		//for(int i=0;i<e.size();i++) e[i].flow = (i&1) ? e[i].cap : 0;
 		s = _s; t = _t;
-		F res = 0;
-		vector<size_t> start(n);
-		while(bfs()) {
-			fill(all(start), 0);
+		F ret = 0;
+		vector<int> ptr, q(n);
+		do {
+			fill(all(lvl), 0);
+			lvl[s] = 1;
+			int qi = 0, qe = lvl[s] = 1;
+			q[0] = s;
+			while(qi < qe && !lvl[t]) {
+				int u = q[qi++];
+				for(int id = beg[u]; ~id; id = g[id].nxt) {
+					auto &e = g[id];
+					if( !lvl[e.to] && not zero(e.f()) )
+						q[qe++] = e.to, lvl[e.to] = lvl[u] + 1;
+				}
+			}
 			F f;
-			while( not zero(f = dfs(s, INF, start)) )
-				res += f;
-		}
-		return res;
+			ptr = beg;
+			while(not zero( f = dfs(s, INF, ptr) ) ) 
+				ret += f;
+		} while(lvl[t]);
+		return ret;
 	}
 };
