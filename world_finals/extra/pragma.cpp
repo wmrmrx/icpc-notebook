eaa3b3dc80046b95058b6baf0a7a260001b37415
LipArcanjo <luanicaroarcanjo@gmail.com>
2024-05-12 23:09:55 -0300

    Add vimrc on pragma file

--- world_finals/extra/pragma.cpp
+++ world_finals/extra/pragma.cpp
@@ -4,3 +4,26 @@
 mt19937_64 rng(123);
 shuffle(permutation.begin(), permutation.end(), rng);
 uniform_int_distribution<int>(a,b)(rng);
+/*
+syntax on
+
+set nowrap
+set tabstop=4
+set shiftwidth=4
+set nocompatible
+set shortmess+=I
+set number
+set relativenumber
+set laststatus=2
+set hidden
+set ignorecase
+set smartcase
+set incsearch
+set mouse+=a
+set backspace=indent,eol,start
+set autoindent
+set wildmenu
+set wildignorecase
+imap jj <Esc>
+
+*/
