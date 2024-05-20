223ef5c755abf4a445da2631dedf56eb0b0bd66c
wmrmrx <wmrmrx@gmail.com>
2024-05-19 21:41:53 -0300

    move

--- old-code/extra/clock.cpp
+++
@@ -1,5 +0,0 @@
-// Clock
-clock_t startTime = clock();
-double getCurrentTime() {
-	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
-}
--- old-code/extra/pragma.cpp
+++
@@ -1,3 +0,0 @@
-// Pragmas
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
