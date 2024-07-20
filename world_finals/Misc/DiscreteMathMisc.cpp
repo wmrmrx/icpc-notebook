cac3536d332f92f25cb3c914a4c113d147a26424
LipArcanjo <luanicaroarcanjo@gmail.com>
2024-07-20 20:25:32 -0300

    Add misc

---
+++ world_finals/Misc/DiscreteMathMisc.cpp
@@ -0,0 +1,72 @@
+int mul(int a, int b, int mod){
+	return a*b%mod;
+}
+
+int pw(int a, int n, int mod){
+	int ret = 1;
+	while(n > 0){
+		if(n&1) ret = mul(ret,a,mod);
+		a = mul(a,a,mod);
+		n /= 2;
+	}
+	return ret;
+}
+
+bool isPrime(int n){
+	if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+	int A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+	int s = __builtin_ctzll(n-1), d = n >> s;
+	for(int a : A){
+		int p = pw(a%n,d,n), i = s;
+		while (p != 1 && p != n - 1 && a % n && i--)
+			p = mul(p, p, n);
+		if (p != n-1 && i != s) return false;
+	}
+	return true;
+}
+
+int pollard(int n){
+	int x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+	auto f = [&](int x) { return modmul(x, x, n) + i; };
+	while (t++ % 40 || __gcd(prd, n) == 1) {
+		if (x == y) x = ++i, y = f(x);
+		if ((q = mul(prd, max(x,y) - min(x,y), n))) prd = q;
+		x = f(x), y = f(f(y));
+	}
+	return __gcd(prd, n);
+}
+
+vector<int> factor(int n){
+	if (n == 1) return {};
+	if (isPrime(n)) return {n};
+	int x = pollard(n);
+	auto l = factor(x), r = factor(n / x);
+	l.insert(l.end(), all(r));
+	return l;
+}
+//O(log^2) on worst, O(log) on average
+int sqrt(int a, int p){
+	a %= p; if (a < 0) a += p;
+	if (a == 0) return 0;
+	assert(pw(a, (p-1)/2, p) == 1); // else no solution
+	if (p % 4 == 3) return pw(a, (p+1)/4, p);
+	int s = p - 1, n = 2;
+	int r = 0, m;
+	while (s % 2 == 0)
+		++r, s /= 2;
+	while (pw(n, (p - 1) / 2, p) != p - 1) ++n;
+	int x = pw(a, (s + 1) / 2, p);
+	int b = pw(a, s, p), g = pw(n, s, p);
+	for (;; r = m) {
+		int t = b;
+		for (m = 0; m < r && t != 1; ++m)
+			t = t * t % p;
+		if (m == 0) return x;
+		int gs = pw(g, 1LL << (r - m - 1), p);
+		g = gs * gs % p;
+		x = x * gs % p;
+		b = b * g % p;
+	}
+	assert(0);
+	return -1;
+}
---
+++ world_finals/Misc/ManhattanMST.h
@@ -0,0 +1,24 @@
+typedef point<int> P;
+
+vector<array<int, 3>> manhattanMST(vector<P> ps) {
+	vi id(ps.size());
+	iota(all(id), 0);
+	vector<array<int, 3>> edges;
+	for(int k = 0; k < 4; k++){
+		sort(all(id), [&](int i, int j) {
+		     return (ps[i]-ps[j]).x < (ps[j]-ps[i]).y;});
+		map<int, int> sweep;
+		for (int i : id) {
+			for (auto it = sweep.lower_bound(-ps[i].y);
+				        it != sweep.end(); sweep.erase(it++)) {
+				int j = it->second;
+				P d = ps[i] - ps[j];
+				if (d.y > d.x) break;
+				edges.push_back({d.y + d.x, i, j});
+			}
+			sweep[-ps[i].y] = i;
+		}
+		for (P& p : ps) if (k & 1) p.x = -p.x; else swap(p.x, p.y);
+	}
+	return edges;
+}
