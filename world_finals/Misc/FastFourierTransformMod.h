33c21bc520a058d1380d1c868f6bcc05ca586680
LipArcanjo <luanicaroarcanjo@gmail.com>
2024-07-20 22:55:13 -0300

    Remove comments

--- world_finals/Misc/FastFourierTransformMod.h
+++ world_finals/Misc/FastFourierTransformMod.h
@@ -1,15 +1,7 @@
 /**
- * Author: chilli
- * Date: 2019-04-25
- * License: CC0
- * Source: http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf
  * Description: Higher precision FFT, can be used for convolutions modulo arbitrary integers
  * as long as $N\log_2N\cdot \text{mod} < 8.6 \cdot 10^{14}$ (in practice $10^{16}$ or higher).
  * Inputs must be in $[0, \text{mod})$.
- * Time: O(N \log N), where $N = |A|+|B|$ (twice as slow as NTT or FFT)
- * Status: stress-tested
- * Details: An in-depth examination of precision for both FFT and FFTMod can be found
- * here (https://github.com/simonlindholm/fft-precision/blob/master/fft-precision.md)
  */
 
 template<int M> vector<ll> convMod(const vector<ll> &a, const vector<ll> &b) {
