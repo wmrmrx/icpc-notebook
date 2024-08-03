a61c03b5148e298bcde47d9c2371e144ae7addc6
LipArcanjo <luanicaroarcanjo@gmail.com>
2024-08-03 00:17:16 -0300

    fix

--- world_finals/Misc/Conv2D.cpp
+++ world_finals/Misc/Conv2D.cpp
@@ -1,11 +1,11 @@
-vector<int> multiply(vector<vector<int>> a, vector<vector<int>> b){
+vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b){
 	int na = a.size();
 	int ma = a[0].size();
 	int nb = b.size();
 	int mb = b[0].size();
 	int N = max(na,nb);
 	int M = max(ma,mb);
-	vector<int> A(N*M*2), B(N*M*2);
+	vector<double> A(N*M*2), B(N*M*2);
 	for(int i = 0; i < na; i++){	
 		for(int j = 0; j < ma; j++){
 			A[i*M*2+j] = a[i][j];
@@ -16,14 +16,14 @@ vector<int> multiply(vector<vector<int>> a, vector<vector<int>> b){
 			B[i*M*2+j] = b[i][j];
 		}
 	}
-	vector<int> C = conv(A,B);
+	vector<double> D = conv(A,B);
 	int n = na+nb-1;
 	int m = ma+mb-1;
-	vector<<vector<int>> ret(n, vector<int>(m));
+	vector<vector<int>> ret(n, vector<int>(m));
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
 			int pos = i*M*2+j;
-			ret[i][j] = C[pos];
+			ret[i][j] = D[pos];
 		}
 	}
 	return ret;
