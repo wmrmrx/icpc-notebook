ba2ada3e780c9f34eac9c49d8339f4c37221feea
wmrmrx <wmrmrx@gmail.com>
2024-08-08 22:46:07 -0300

    Add LuanTree

--- world_finals/Graphs/BlockCutTree.cpp
+++ world_finals/Graphs/BlockCutTree.cpp
@@ -45,3 +45,33 @@ struct BlockCutTree {
 		for(int v=0;v<n;v++) if(!vis[v]) dfs(v, 1);
 	}
 };
+
+struct LuanTree: BlockCutTree {
+	vector<vector<int>> blocks, tree; // nodes in block, blockcuttree
+	vector<vector<pair<int,int>>> edgblocks; // edges on  the blocks
+	vector<int> art, pos; // art = [u is articulation point], pos[u] = mapping of vertex in tree, -1 if isolated!
+	LuanTree(int n, int m, vector<pair<int,int>> g[]): BlockCutTree(n, m, g) {
+		pos.assign(n, -1);
+		art.assign(n, 0);
+		blocks.resize(ncomp);
+		edgblocks.resize(ncomp);
+		int nart = 0;
+		for(int u=0;u<n;u++) {
+			art[u] = !gart[u].empty();
+			if(art[u])
+				pos[u] = ncomp + (nart++);
+			else if(not g[u].empty())
+				pos[u] = g[u][0].second;
+		}
+		tree.resize(ncomp + nart);
+		for(int u=0;u<n;u++) for(auto [v, e]: g[u]) {
+			int c = comp[e];
+			if(u < v) edgblocks[c].emplace_back(u, v);
+			blocks[c].pb(u); blocks[c].pb(v);
+		}
+		for(int c=0;c<ncomp;c++) {
+			sort(all(blocks[c]));
+			blocks[c].erase(unique(all(blocks[c])), blocks[c].end());
+		}
+	}
+};
