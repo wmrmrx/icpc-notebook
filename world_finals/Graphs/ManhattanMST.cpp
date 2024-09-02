7b778691c59d45591185a7597b144f9108f9989b
wmrmrx <wmrmrx@gmail.com>
2024-09-02 20:47:55 -0300

    Add ManhattanMST.cpp

--- world_finals/Graphs/ManhattanMST.cpp
+++ world_finals/Graphs/ManhattanMST.cpp
@@ -1,24 +1,39 @@
-typedef point<int> P;
+// Computes O(n) edges that contains all the edges of the  Manhattan MST
+// of a set of points int the 2D plane. Must apply Kruskal to these edges
+// to obtain the actual MST.
+//
+// xs - (input) coordinates in the x-axis
+// ys - (input) coordinates in the y-axis
+// E - superset of the manhattan MST edges
+//
+// Complexity: O(n logn)	
 
-vector<array<int, 3>> manhattanMST(vector<P> ps) {
-	vector<int> id(ps.size());
-	iota(all(id), 0);
-	vector<array<int, 3>> edges;
-	for(int k = 0; k < 4; k++){
-		sort(all(id), [&](int i, int j) {
-		     return (ps[i]-ps[j]).x < (ps[j]-ps[i]).y;});
-		map<int, int> sweep;
-		for (int i : id) {
-			for (auto it = sweep.lower_bound(-ps[i].y);
-				        it != sweep.end(); sweep.erase(it++)) {
-				int j = it->second;
-				P d = ps[i] - ps[j];
-				if (d.y > d.x) break;
-				edges.push_back({d.y + d.x, i, j});
+vector<pair<ll, pair<int, int>> > E;
+
+void manhattan_mst(vector<ll> xs, vector<ll> ys){
+	int n = sz(xs);
+	vector<int> ord(n);
+	for(int i = 0; i < n; i++)
+		ord[i]=i;
+	for(int s = 0; s < 2; s++) {
+		for(int t = 0; t < 2; t++) {
+			auto cmp = [&](int i, int j) -> bool {
+				if(xs[i] + ys[i] == xs[j] + ys[j]) return ys[i] < ys[j];
+				return xs[i] + ys[i] < xs[j] + ys[j];
+			};
+			sort(ord.begin(), ord.end(), cmp);
+			map<int, int> id;
+			for(int i: ord) {
+				for(auto it = id.lower_bound(-ys[i]); it != id.end(); it = id.erase(it)) {
+					int j = it->ss;
+					if(xs[j] - ys[j] > xs[i] - ys[i]) break;
+					E.pb(mp(abs(xs[i] - xs[j]) + abs(ys[i] - ys[j]), mp(i, j)));
+				}
+				id[-ys[i]] = i;
 			}
-			sweep[-ps[i].y] = i;
+			swap(xs,ys);
 		}
-		for (P& p : ps) if (k & 1) p.x = -p.x; else swap(p.x, p.y);
+		for(int i = 0; i < n; i++)
+			xs[i] *= -1;
 	}
-	return edges;
 }
