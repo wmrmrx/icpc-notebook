7fd360946e06ab58946dfa2a1870b0bae269fde9
Willian Mori <wmrmrx@gmail.com>
2025-08-26 20:37:37 -0300

    remove vimrc

--- world_finals/Extra/Extra.cpp
+++ world_finals/Extra/Extra.cpp
@@ -4,9 +4,3 @@
 mt19937_64 rng(123);
 shuffle(permutation.begin(), permutation.end(), rng);
 uniform_int_distribution<int>(a,b)(rng);
-/*
-.vimrc
-syntax on
-set nowrap ts=4 sw=4 nocp shm+=I nu rnu ls=2 hid ic scs mouse+=a bs=indent,eol,start ai wmnu wic
-imap jj <Esc>
-*/
